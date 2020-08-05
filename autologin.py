7df54cb2cfae6bc3302ec4dde6246e60b17a3acf
Vaishu2001 <66245171+Vaishu2001@users.noreply.github.com>
2020-08-05 13:25:58 +0530

    Update autologin.py

--- autologin.py
+++ autologin.py
@@ -1,3 +1,17 @@
+''' Automatic login in Facebook
+steps to follow:
+We’ll need to install a couple of things:
+1)Selenium, which allows you to control browsers from Python
+2)ChromeDriver, which allows software to control Chrome (like Selenium!)
+1)Open the browser
+2)provide facebook link
+3)Store the email and password in user_id and password variables
+4)Finding  the email id in the source code and passing my user_id as a parameter #source code is entire code in which a webpage is written
+5)Finding the password id in the source code and passing my password as a parameter
+6)Finding the login id in the souce code 
+7)clicking the login button'''
+
+
 from selenium import webdriver
 browser=webdriver.Chrome("C:\\Users\\ASUS\\Desktop\\chromedriver.exe")
 browser.get("https://www.facebook.com")
